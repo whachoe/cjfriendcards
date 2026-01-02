a68e0c006d958b072de4e63e91fe0cd5f36ff071
Jo Giraerts <whachoe@gmail.com>
2026-01-02 15:32:29 +0100

    fix styling `New Card`

--- cjFriendCards/resources/views/layout.blade.php
+++ cjFriendCards/resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://unpkg.com/htmx.org"></script>
 </head>
-<body class="bg-primary-light">
+<body class="bg-primary-light">    
     <nav class="bg-primary-dark shadow">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16">
@@ -18,7 +18,7 @@
                 <div class="flex items-center space-x-4">
                     <a href="{{ route('cards.index') }}" class="text-primary-light hover:text-primary-accent">Cards</a>
                     <a href="{{ route('cards.birthday-calendar') }}" class="text-primary-light hover:text-primary-accent">Birthdays</a>
-                    <a href="{{ route('cards.create') }}" class="bg-primary-accent text-white px-4 py-2 rounded hover:brightness-110 transition">New Card</a>
+                    <a href="{{ route('cards.create') }}" class="bg-primary-accent rounded hover:brightness-110 transition">New Card</a>
                 </div>
             </div>
         </div>
